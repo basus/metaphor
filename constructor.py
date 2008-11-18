c3f2689a8eda45e8a3631d1536d5db420ea1b58b
Shrutarshi Basu <basu@bytebaker.com>
2008-11-18 04:03:54 +0000

    Updates to Metaphor and Euler 17. Added a Prolog directory and started on uML interpreter

--- constructor.py
+++ constructor.py
@@ -6,6 +6,22 @@ class Pattern:
         self.name = name
         self.terms = terms
         self.nonterms = nonterms
+        self.productions = []
+        self.defer = []
+        self.assign = {}
+        self.expansions = {}
+
+    def build_productions(self):
+        for production in self.productions:
+            if not self.expansions.has_key(production[0]):
+                self.expansions[production[0]] = {}
+            nonterm = self.expansions[production[0]]
+            nonterm[production[1]] = production[2]
+
+    def transform(self, token):
+        legals = self.expansions[token]
+        
+
 
 class Constructor:
     ''' A class factory that takes the AST from the Parser and the environments
@@ -14,13 +30,50 @@ class Constructor:
     def __init__(self, astree, env_patterns):
         self.astree = astree
         self.env_patterns = env_patterns
-        self.pattern_list = []
+        self.patterns = {}
 
     def create(self):
         for name,value in self.env_patterns():
             new_pattern = Pattern(name,
                                   value[list(set("terms"))],
                                   value[list(set("nonterms"))])
-            self.pattern_list.append(new_pattern)
+            self.patterns[name] = new_pattern
 
-    def 
+    def scan_tree(self):
+        root = self.astree.root
+        toplevels = root.children
+        for each_toplevel in toplevels:
+            constructs = self.astree.get_children(each_toplevel)
+            self.current = self.patterns[constructs[0]]
+            if 'is' in constructs:
+                parent = each.toplevel.children[1].children[0]
+                clone(parent)
+            for construct in each_toplevel.children:
+                if construct.data == 'Axiom':
+                    self.current.axiom == construct.children[0].data
+                if construct.data == 'Production':
+                    self.production(construct)
+                if construct.data == 'Defer':
+                    self.current.defer.append(construct.children[0].data)
+                if construct.data == 'Assign':
+                    param = construct.children[0].data
+                    self.current.assign[param] = int(construct.children[0].data)
+
+
+    def clone(parent):
+        tempname = self.current.name
+        self.current = copy.deepcopy(self.patterns[parent.data])
+        self.current.name = tempname
+
+    def production(self, top):
+        pnt = top.children[0]
+        plist = top.children[1]
+        if pnt.data == 'pnt':
+            nonterm = pnt.children[0].data
+            param = pnt.children[0].data
+        else:
+            nonterm = pnt.data
+            param = ''
+        productions = [x.data for x in plist.children]
+        self.current.productions.append(tuple(nonterm, param, productions))
+
