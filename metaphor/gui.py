99abac71bd3f07b87f78c8883d0a4d6aa4992d2b
Shrutarshi <basu@bytebaker.com>
2009-02-15 21:52:37 -0500

    Fixed Metaphor GUI to generate and display grammars
    QtGui now correctly displays list of grammars generated

--- metaphor/gui.py
+++ metaphor/gui.py
@@ -1,16 +1,33 @@
-from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 from qtui import Ui_MainWindow
 import sys
 import os
-import codecs
+
 
 import syntax
 import semantics
-                     
 
-class QtInterface(QtGui.QMainWindow):
+class GrammarList(QAbstractListModel):
+    def __init__(self, grammars, parent=None, *args):
+        QAbstractListModel.__init__(self, parent, *args)
+        self.grammars = grammars
+
+    def rowCount(self, parent):
+        return len(self.grammars)
+
+    def columnCount(self, parent):
+        return 1
+
+    def data(self, index, role):
+        if index.isValid() and role == Qt.DisplayRole:
+            return QVariant(self.grammars[index.row()])
+        else:
+            return QVariant()
+
+class QtInterface(QMainWindow):
     def __init__(self):
-        QtGui.QDialog.__init__(self)
+        QDialog.__init__(self)
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -20,24 +37,23 @@ class QtInterface(QtGui.QMainWindow):
 
     def connections(self):
         self.connect(self.ui.actionOpen,
-                     QtCore.SIGNAL("activated()"),
+                     SIGNAL("activated()"),
                      self.slot_open_file)
 
         self.connect(self.ui.actionSaveAs,
-                     QtCore.SIGNAL("activated()"),
+                     SIGNAL("activated()"),
                      self.slot_saveas_file)
 
         self.connect(self.ui.actionSave,
-                     QtCore.SIGNAL("activated()"),
+                     SIGNAL("activated()"),
                      self.slot_save_current)
 
         self.connect(self.ui.compile,
-                     QtCore.SIGNAL("clicked()"),
+                     SIGNAL("clicked()"),
                      self.slot_compile)
 
     def slot_compile(self):
         file = open(self.filename,'w')
-        print self.ui.GrammarEdit.toPlainText()
         file.write(self.ui.GrammarEdit.toPlainText())
         file.close()
         file = open(self.filename)
@@ -46,18 +62,24 @@ class QtInterface(QtGui.QMainWindow):
         builder = semantics.Builder(tree)
         self.env = semantics.Environment(builder)
         self.grammars = self.env.populate()
-        for grammar in self.grammars:
-            self.guigrammars.append(QtGui.QListViewItem(self.ui.Grammars, grammar))
+        print self.grammars
+        self.glist = GrammarList(self.grammars, self)
+        self.ui.Grammars.setModel(self.glist)
+        print self.glist
+#        self.ui.Grammars.show()
+        print "Grammars displayed"
+#        for grammar in self.grammars:
+#            self.guigrammars.append(QListViewItem(self.ui.Grammars, grammar))
         
 
     def slot_open_file(self):
-        self.filename = QtGui.QFileDialog.getOpenFileName(self, "Open File",os.getcwd(), "*.gr")
+        self.filename = QFileDialog.getOpenFileName(self, "Open File",os.getcwd(), "*.gr")
         file = open(self.filename)
         data = file.read()
         self.ui.GrammarEdit.setText(data)
 
     def slot_saveas_file(self):
-        self.filename = QtGui.QFileDialog.getSaveFileName(self, "Save File",os.getcwd(), "*.gr")
+        self.filename = QFileDialog.getSaveFileName(self, "Save File",os.getcwd(), "*.gr")
         file = open(self.filename,'w','utf-8')
         file.write(self.ui.GrammarEdit.toPlainText())
         file.close()
@@ -66,7 +88,7 @@ class QtInterface(QtGui.QMainWindow):
         pass
         
 
-app = QtGui.QApplication(sys.argv)
+app = QApplication(sys.argv)
 window = QtInterface()
 
 
