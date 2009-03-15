6680e299d2adff14823d6a0bf4bb19a8bcd463ec
Shrutarshi <basu@bytebaker.com>
2009-03-15 10:57:59 -0400

    Added Changed lsys files.

--- examples/lsys.py
+++ examples/lsys.py
@@ -4,4 +4,4 @@ context = 'contexts/turtle.py'
 
 generate = [('Dragon', 10), ('KochCurve',5)]
 
-render = ['d.png', ('k.png', 'contexts/turtle.py')]
+render = ['dragon.png', ('curve.png', 'contexts/turtle.py')]
