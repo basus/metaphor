91ff9357ee3c1f0f4a4d24c530d3ff700adb02fe
Shrutarshi Basu <basus@jazz701.cs.lafayette.edu>
2009-04-07 19:16:06 -0400

    Added grammar for simplest L-system
    Turtle context also had to be changed to allow the images to fit on screen. Really need SVGs.

--- examples/ncur.py
+++ examples/ncur.py
@@ -7,11 +7,20 @@ generate = [('Sierpinski2', 2),
             ('Sierpinski6', 6),
             ('Sierpinski9', 9)]
 
+for x in range(1, 7):
+    tup = ('Simple', x*5)
+    generate.append(tup)
+
 
 render = []
 
 for x in range(1,5):
     fname = "tri"+str(x)+".png"
     render.append(fname)
+
+for x in range(1,7):
+    fname = "simp"+str(x)+".png"
+    render.append(fname)
+
     
 #render = ['dragon.png', ('curve.png', 'contexts/turtle.py')]
