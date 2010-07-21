86903942f839a611a508d91954e2a91b113b3fbe
Shrutarshi Basu <shr@basushr.net>
2010-07-21 16:16:20 -0400

    Added HTML documentation auto-generated from org-mode

---
+++ doc/publish.el
@@ -0,0 +1,17 @@
+(require 'org-publish)
+(setq org-publish-project-alist
+      '(
+        ("to-html"
+         :base-directory "~/src/metaphor/doc/org"
+         :base-extension "org"
+         :publishing-directory "~/src/metaphor/doc/html"
+         :recursive t
+         :publishing-function org-publish-org-to-html
+         :exclude "level-0.org"
+         :headline-levels 4
+         :table-of-contents: nil
+         :auto-preamble t
+         :auto-postamble nil
+         )
+        ("metaphor" :components ("to-html"))
+        ))
