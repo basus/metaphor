4da0c2e8b2206cc47a00924bd4e272b4bb5829c3
Shrutarshi Basu <shr@basushr.net>
2010-11-29 23:01:50 -0500

    Added copying information and License

--- examples/ncur.py
+++ examples/ncur.py
@@ -1,3 +1,18 @@
+# This file is part of Metaphor.
+
+# Metaphor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Metaphor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Metaphor.  If not, see <http://www.gnu.org/licenses/>.
+
 compile = 'examples/ncur.gr'
 
 context = 'contexts/turtle.py'
@@ -22,5 +37,3 @@ for x in range(1,7):
     fname = "simp"+str(x)+".png"
     render.append(fname)
 
-    
-#render = ['dragon.png', ('curve.png', 'contexts/turtle.py')]
