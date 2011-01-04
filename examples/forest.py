a39dafc0401021a267751ba095b1dce5811b0d7d
Shrutarshi Basu <shr@basushr.net>
2011-01-04 14:15:14 +0530

    Updated forest example to new syntax

--- examples/forest.py
+++ examples/forest.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Metaphor.  If not, see <http://www.gnu.org/licenses/>.
 
-compile = 'examples/forest.gr'
+compile = 'examples/forest.lsys'
 
 context = 'contexts/turtle.py'
 
